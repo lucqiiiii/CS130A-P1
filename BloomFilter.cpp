d2905d811b0c1f49203cab96c752bc817ddbac48
lucqiiiii <qi_guo@umail.ucsb.edu>
2019-05-04 00:46:23 -0700

    done

--- BloomFilter.cpp
+++ BloomFilter.cpp
@@ -1,1 +1,33 @@
 #include "BloomFilter.h"
+#include "string"
+#include "vector"
+
+
+BloomFilter::BloomFilter(int k, int m, string strfn, string intfn){
+  this -> k = k;
+  this -> m = m;
+  this -> strfn = strfn;
+  this -> intfn = intfn;
+  bits = new vector<uint64_t>[m];
+  for(int i = 0; i < m; i++){
+    bits[i] = 0;
+  }
+}
+
+BloomFilter::~BloomFilter(){
+  delete []bits;
+
+}
+
+void BloomFilter::insert(const string& value){
+  int key = intfn::hash(strfn::hash(value));
+  bits[key] = 1;
+  
+}
+
+bool BloomFilter::lookup(const string& value) const{
+
+
+}
+
+
