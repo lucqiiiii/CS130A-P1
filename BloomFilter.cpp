5029fde0e706a8ea3e353bc1c8dac82c134037b2
lucqiiiii <qi_guo@umail.ucsb.edu>
2019-05-04 15:38:14 -0700

    done

--- BloomFilter.cpp
+++ BloomFilter.cpp
@@ -1,33 +1,47 @@
 #include "BloomFilter.h"
 #include "string"
 #include "vector"
-
+#include "IntegerHashes.h"
+#include "StringHashes.h"
 
 BloomFilter::BloomFilter(int k, int m, string strfn, string intfn){
   this -> k = k;
   this -> m = m;
-  this -> strfn = strfn;
-  this -> intfn = intfn;
-  bits = new vector<uint64_t>[m];
-  for(int i = 0; i < m; i++){
-    bits[i] = 0;
+  if(strfn == "jenkins"){
+    this -> strfn = new JenkinsHash();
   }
+  else if(strfn == "pearson"){
+    this -> strfn = new PearsonHash();
+  }
+  if(intfn == "division"){
+    this -> intfn = new DivisionHash();
+  }
+  else if(intfn == "reciprocal"){
+    this -> intfn = new ReciprocalHash();
+  }
+  else if(intfn == "squareroot"){
+    this -> intfn = new SquareRootHash();
+  }
+  bits = new vector<uint64_t>(m,0);
 }
 
 BloomFilter::~BloomFilter(){
   delete []bits;
-
+  delete strfn;
+  delete intfn;
 }
 
 void BloomFilter::insert(const string& value){
-  int key = intfn::hash(strfn::hash(value));
-  bits[key] = 1;
-  
+  uint64_t key = intfn -> hash(strfn -> hash(value));
+  bits[key/64] |= (1 << (key % 64));
 }
 
 bool BloomFilter::lookup(const string& value) const{
-
-
+  uint64_t key = intfn -> hash(strfn -> hash(value));
+  if((bits[key/64] &= (1 << (key % 64))) == 1){
+    return true;
+  }
+  return false;
 }
 
 
--- HashSet.cpp
+++ HashSet.cpp
@@ -1,1 +1,58 @@
 #include "HashSet.h"
+#include "string"
+#include "vector"
+#include "IntegerHashes.h"
+#include "StringHashes.h"
+
+HashSet::HashSet(){
+  this -> nitems = 0;
+  this -> nslots = 101;
+  slots = new vector<string*>(nslots,0);
+  this -> strfn = new JenkinsHash();
+  this -> intfn = new SquareRootHash();
+}
+
+HashSet::~HashSet(){
+  delete [](*slots);
+  delete slots;
+  delete strfn;
+  delete intfn;
+}
+
+void HashSet::insert(const std::string& value){
+  uint64_t key = intfn -> hash(strfn -> hash(value));
+  while((*slots)[key] != NULL && (*slots)[key] != value){
+    key++;
+  }
+  if((*slots)[key] == value){
+    return;
+  }
+  if((*slots)[key] == NULL){
+    *slots[key] = value;
+    nitems++;
+  }
+  if(++nitems > (nslots / 2)){
+    rehash();
+  }
+}
+
+bool HashSet::lookup(const std::string& value) const{
+  uint64_t key = intfn -> hash(strfn -> hash(value));
+  while((*slots)[key] != NULL){
+    if((*slots)[key] == value){
+      return true;
+    }
+    key++;
+  }
+  return false;
+}
+
+void HashSet::rehash(){
+  string* oldslots = *slots;
+  slots = new vector<string*>(nslots*2,0);
+  for(int i = 0; i < nslots; i++){
+    (*slots)[i] = oldslots[i];
+  }
+  delete []oldslots;
+  nslots = 2 * nslots; 
+} 
