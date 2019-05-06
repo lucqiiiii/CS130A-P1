45dca28c9159db0a68bc072feb93a8d66fdccb15
lucqiiiii <qi_guo@umail.ucsb.edu>
2019-05-05 23:29:40 -0700

    done

--- HashSet.cpp
+++ HashSet.cpp
@@ -26,6 +26,9 @@ void HashSet::insert(const std::string& value){
   while(slots[key] != NULL && *(slots[key]) != value){
     key++;
   }
+  if(key > nslots){
+    rehash();
+  }
   if(slots[key] == NULL){
     *(slots[key]) = value;
     nitems++;
