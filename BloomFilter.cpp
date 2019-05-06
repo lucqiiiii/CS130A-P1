bc485a59562830b909cc5f7e2660dbdf1f657a60
lucqiiiii <qi_guo@umail.ucsb.edu>
2019-05-05 18:31:31 -0700

    done

--- BloomFilter.cpp
+++ BloomFilter.cpp
@@ -14,13 +14,22 @@ BloomFilter::BloomFilter(int k, int m, string strfn, string intfn){
     this -> strfn = new PearsonHash();
   }
   if(intfn == "division"){
-    this -> intfn = new DivisionHash();
+    this -> intfn = new DivisionHash*[k];
+    for(int i = 0; i < k; i++){
+      intfn[i] = new DivisionHash(i,m);
+    }
   }
   else if(intfn == "reciprocal"){
-    this -> intfn = new ReciprocalHash();
+    this -> intfn = new ReciprocalHash*[k];
+    for(int i = 0; i < k; i++){
+      intfn[i] = new ReciprocalHash(i,m);
+    }
   }
   else if(intfn == "squareroot"){
-    this -> intfn = new SquareRootHash();
+    this -> intfn = new SquareRootHash*[k];
+    for(int i = 0; i < k; i++){
+      intfn[i] = new SquareRootHash(i,m);
+    }
   }
   bits = new vector<uint64_t>(m,0);
 }
@@ -28,20 +37,27 @@ BloomFilter::BloomFilter(int k, int m, string strfn, string intfn){
 BloomFilter::~BloomFilter(){
   delete []bits;
   delete strfn;
+  delete [](*intfn);
   delete intfn;
 }
 
 void BloomFilter::insert(const string& value){
-  uint64_t key = intfn -> hash(strfn -> hash(value));
-  bits[key/64] |= (1 << (key % 64));
+  uint64_t key;
+  for(int i = 0; i < k; i++){
+    key = intfn[i] -> hash(strfn -> hash(value));
+    bits[key/64] |= (1 << (key % 64));
+  }
 }
 
 bool BloomFilter::lookup(const string& value) const{
-  uint64_t key = intfn -> hash(strfn -> hash(value));
-  if((bits[key/64] &= (1 << (key % 64))) == 1){
-    return true;
+  uint64_t key;
+  for(int i = 0; i < k; i++){
+    key = intfn[i] -> hash(strfn -> hash(value));
+    if((bits[key/64] &= (1 << (key % 64))) != 1){
+      return false;
+    }
   }
-  return false;
+  return true;
 }
 
 
